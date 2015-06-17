583c8ba3b2699e8d5401e218d18fe7765c21b0b2
Lihan Li <frankieteardrop@gmail.com>
2015-06-17 18:13:28 -0400

    dont wait for page load to add el

--- dist/js/overlay_spinner.js
+++ dist/js/overlay_spinner.js
@@ -1,9 +1,7 @@
 (function() {
-  var $spinner = $('<div class="overlay-spinner"></div>');
-
-  $(function() {
-    $('body').append($spinner);
-  });
+  var $spinner = $('<div class="overlay-spinner"></div>').hide();
+  // must add the script after creating the body tag
+  $('body').append($spinner);
 
   window.toggleOverlaySpinner = function(show) {
     $spinner[show ? 'show' : 'hide']();
